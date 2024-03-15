ff763242fa7c5bfe872f6c84408e29f5c788cad2
amaramci <amarmujezinovic1@gmail.com>
2024-03-15 15:54:11 +0100

    neki commit

--- pages/my-wallet.js
+++ pages/my-wallet.js
@@ -8,7 +8,20 @@ import images from "../img";
 const novcanik = () =>{
     return (
         <div className={Style.novcanik}>
-
+            <h1>Vaši NFTi</h1>
+            <div className={Style.novcanik_box}>
+                <div className={Style.novcanik_box_nft}>
+                    <Link href={{pathname: "/about-nft1"}}>
+                        <Image src={images.nftimage1} width={200} height={200} className={Style.novcanik_box_nft_img}/>
+                    </Link>
+                    <Link href={{pathname: "/about-nft2"}}>
+                        <Image src={images.nftimage2} width={200} height={200} className={Style.novcanik_box_nft_img}/>
+                    </Link>
+                    <Link href={{pathname: "/about-nft3"}}>
+                        <Image src={images.nftimage3} width={200} height={200} className={Style.novcanik_box_nft_img}/>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
