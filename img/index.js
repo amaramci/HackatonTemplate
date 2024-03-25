dc07c2841c59e29f332842831dd62484287951f6
amaramci <amarmujezinovic1@gmail.com>
2024-03-25 23:36:40 +0100

    blizu kraja

--- img/index.js
+++ img/index.js
@@ -8,6 +8,20 @@ import user3 from "./user-3.png";
 import user4 from "./user-4.png";
 import user8 from "./user-8.png";
 import user9 from "./user-9.png";
+import epos from "./Elektronsko_Poslovanje.png";
+import baze from "./Baze_Podataka.png";
+import ekonomija from "./Ekonomija.png";
+import mata1 from "./Matematika_1.png";
+import mata2 from "./Matematika_2.png";
+import mndz from "./Menadžment.png";
+import principi from "./Principi_Programiranja.png";
+import prog1 from "./Programiranje_1.png";
+import psiho from "./Psihologija.png";
+import stat from "./Statistika.png";
+import vestacka from "./Veštacka_Inteligencija.png";
+import zavrsni from "./Završni_Rad.png";
+
 
 export default {creatorbackground5, creatorbackground6, hero, nftimage1, nftimage2,
-nftimage3, user3, user4, user8, user9};
+nftimage3, user3, user4, user8, user9, epos, baze, ekonomija, mata1, mata2, mndz, principi,
+prog1, psiho, stat, vestacka, zavrsni};
--- pages/kolekcije.js
+++ pages/kolekcije.js
@@ -12,7 +12,7 @@ const kolekcije = () =>{
                 <div className={Style.kolekcije_box_box}>
                     <div className={Style.kolekcije_box_box_image}>
                         <Link href={{pathname: "/about-programiranje1"}}>
-                            <Image src={images.creatorbackground5} width={450} height={350} className={Style.kolekcije_box_box_img}/>
+                            <Image src={images.prog1} width={450} height={350} className={Style.kolekcije_box_box_img}/>
                         </Link>
                     </div>
                     <div className={Style.kolekcije_box_box_info}>
@@ -22,7 +22,7 @@ const kolekcije = () =>{
                 <div className={Style.kolekcije_box_box}>
                     <div className={Style.kolekcije_box_box_image}>
                         <Link href={{pathname: "/about-elektronsko-poslovanje"}}>
-                            <Image src={images.creatorbackground6} width={450} height={350} className={Style.kolekcije_box_box_img}/>
+                            <Image src={images.epos} width={450} height={350} className={Style.kolekcije_box_box_img}/>
                         </Link>
                     </div>
                     <div className={Style.kolekcije_box_box_info}>
@@ -32,21 +32,101 @@ const kolekcije = () =>{
                 <div className={Style.kolekcije_box_box}>
                     <div className={Style.kolekcije_box_box_image}>
                         <Link href={{pathname: "/about-osnove-kvaliteta"}}>
-                            <Image src={images.creatorbackground6} width={450} height={350} className={Style.kolekcije_box_box_img}/>
+                            <Image src={images.baze} width={450} height={350} className={Style.kolekcije_box_box_img}/>
                         </Link>
                     </div>
                     <div className={Style.kolekcije_box_box_info}>
-                        <h2>Osnove kvaliteta</h2>
+                        <h2>Baze podataka</h2>
                     </div>
                 </div>
                 <div className={Style.kolekcije_box_box}>  
                     <div className={Style.kolekcije_box_box_image}>
                         <Link href={{pathname: "/about-menadzment"}}>
-                            <Image src={images.creatorbackground5} width={450} height={350} className={Style.kolekcije_box_box_img}/>
+                            <Image src={images.ekonomija} width={450} height={350} className={Style.kolekcije_box_box_img}/>
+                        </Link>
+                    </div>
+                    <div className={Style.kolekcije_box_box_info}>
+                        <h2>Ekonomija</h2>
+                    </div>
+                </div>
+                <div className={Style.kolekcije_box_box}>  
+                    <div className={Style.kolekcije_box_box_image}>
+                        <Link href={{pathname: "/about-menadzment"}}>
+                            <Image src={images.mata1} width={450} height={350} className={Style.kolekcije_box_box_img}/>
+                        </Link>
+                    </div>
+                    <div className={Style.kolekcije_box_box_info}>
+                        <h2>Matematika 1</h2>
+                    </div>
+                </div>
+                <div className={Style.kolekcije_box_box}>  
+                    <div className={Style.kolekcije_box_box_image}>
+                        <Link href={{pathname: "/about-menadzment"}}>
+                            <Image src={images.mata2} width={450} height={350} className={Style.kolekcije_box_box_img}/>
+                        </Link>
+                    </div>
+                    <div className={Style.kolekcije_box_box_info}>
+                        <h2>Matematika 2</h2>
+                    </div>
+                </div>
+                <div className={Style.kolekcije_box_box}>  
+                    <div className={Style.kolekcije_box_box_image}>
+                        <Link href={{pathname: "/about-menadzment"}}>
+                            <Image src={images.psiho} width={450} height={350} className={Style.kolekcije_box_box_img}/>
+                        </Link>
+                    </div>
+                    <div className={Style.kolekcije_box_box_info}>
+                        <h2>Psihologija</h2>
+                    </div>
+                </div>
+                <div className={Style.kolekcije_box_box}>  
+                    <div className={Style.kolekcije_box_box_image}>
+                        <Link href={{pathname: "/about-menadzment"}}>
+                            <Image src={images.stat} width={450} height={350} className={Style.kolekcije_box_box_img}/>
+                        </Link>
+                    </div>
+                    <div className={Style.kolekcije_box_box_info}>
+                        <h2>Statistika</h2>
+                    </div>
+                </div>
+                <div className={Style.kolekcije_box_box}>  
+                    <div className={Style.kolekcije_box_box_image}>
+                        <Link href={{pathname: "/about-menadzment"}}>
+                            <Image src={images.vestacka} width={450} height={350} className={Style.kolekcije_box_box_img}/>
+                        </Link>
+                    </div>
+                    <div className={Style.kolekcije_box_box_info}>
+                        <h2>Veštačka inteligencija</h2>
+                    </div>
+                </div>
+                <div className={Style.kolekcije_box_box}>  
+                    <div className={Style.kolekcije_box_box_image}>
+                        <Link href={{pathname: "/about-menadzment"}}>
+                            <Image src={images.zavrsni} width={450} height={350} className={Style.kolekcije_box_box_img}/>
+                        </Link>
+                    </div>
+                    <div className={Style.kolekcije_box_box_info}>
+                        <h2>Završni rad</h2>
+                    </div>
+                </div>
+                <div className={Style.kolekcije_box_box}>  
+                    <div className={Style.kolekcije_box_box_image}>
+                        <Link href={{pathname: "/about-menadzment"}}>
+                            <Image src={images.mndz} width={450} height={350} className={Style.kolekcije_box_box_img}/>
                         </Link>
                     </div>
                     <div className={Style.kolekcije_box_box_info}>
                         <h2>Menadžment</h2>
+                    </div>
+                </div>
+                <div className={Style.kolekcije_box_box}>  
+                    <div className={Style.kolekcije_box_box_image}>
+                        <Link href={{pathname: "/about-menadzment"}}>
+                            <Image src={images.principi} width={450} height={350} className={Style.kolekcije_box_box_img}/>
+                        </Link>
+                    </div>
+                    <div className={Style.kolekcije_box_box_info}>
+                        <h2>Principi programiranja</h2>
                     </div>
                 </div>
             </div>
